31eefe419ad6f1dfe636ab4110fb2a6ad68c4115
Chayan Das <chayandas0308@gmail.com>
2023-03-06 10:20:53 +0530

    cam added with capture

--- app/src/main/java/com/example/shelfwatchcamdemo/MainActivity.kt
+++ app/src/main/java/com/example/shelfwatchcamdemo/MainActivity.kt
@@ -1,11 +1,194 @@
 package com.example.shelfwatchcamdemo
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.shelfwatchcamdemo.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.Manifest;
+import android.content.ContentValues
+import android.graphics.*
+import android.media.Image
+import android.provider.MediaStore
+import android.util.Log
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var viewBinding: ActivityMainBinding
+
+  private var imageCapture: ImageCapture? = null
+
+  private lateinit var cameraExecutor: ExecutorService
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    viewBinding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(viewBinding.root)
+
+    // Request camera permissions
+    if (allPermissionsGranted()) {
+      startCamera()
+    } else {
+      ActivityCompat.requestPermissions(
+        this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+      )
+    }
+
+    // Set up the listeners for take photo and video capture buttons
+    viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+    cameraExecutor = Executors.newSingleThreadExecutor()
+  }
+
+  private fun takePhoto() {
+    // Get a stable reference of the modifiable image capture use case
+    val imageCapture = imageCapture ?: return
+
+    // Create time stamped name and MediaStore entry.
+    val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+      .format(System.currentTimeMillis())
+    val contentValues = ContentValues().apply {
+      put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+      put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+      if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+        put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+      }
+    }
+
+    // Create output options object which contains file + metadata
+    val outputOptions = ImageCapture.OutputFileOptions
+      .Builder(
+        contentResolver,
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        contentValues
+      )
+      .build()
+
+    // Set up image capture listener, which is triggered after photo has
+    // been taken
+    imageCapture.takePicture(
+      outputOptions,
+      ContextCompat.getMainExecutor(this),
+      object : ImageCapture.OnImageSavedCallback {
+        override fun onError(exc: ImageCaptureException) {
+          Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+        }
+
+        override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+          val msg = "Photo capture succeeded: ${output.savedUri}"
+          Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+          Log.d(TAG, msg)
+        }
+      }
+    )
+
+    imageCapture.takePicture(
+      ContextCompat.getMainExecutor(this),
+      object : ImageCapture.OnImageCapturedCallback() {
+
+        override fun onCaptureSuccess(image: ImageProxy) {
+          super.onCaptureSuccess(image)
+          Log.d("format", "${image.format}")
+          viewBinding.capturedImg.setImageBitmap(image.convertImageProxyToBitmap())
+          image.close()
+        }
+
+        override fun onError(exception: ImageCaptureException) {
+          super.onError(exception)
+          Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+        }
+      }
+    )
+  }
+
+  private fun startCamera() {
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+    cameraProviderFuture.addListener({
+      // Used to bind the lifecycle of cameras to the lifecycle owner
+      val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+      // Preview
+      val preview = Preview.Builder()
+        .build()
+        .also {
+          it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+        }
+
+      imageCapture = ImageCapture.Builder()
+        .build()
+
+      // Select back camera as a default
+      val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+      try {
+        // Unbind use cases before rebinding
+        cameraProvider.unbindAll()
+
+        // Bind use cases to camera
+        cameraProvider.bindToLifecycle(
+          this, cameraSelector, preview, imageCapture
+        )
+
+      } catch (exc: Exception) {
+        Log.e(TAG, "Use case binding failed", exc)
+      }
+
+    }, ContextCompat.getMainExecutor(this))
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    if (requestCode == REQUEST_CODE_PERMISSIONS) {
+      if (allPermissionsGranted()) {
+        startCamera()
+      } else {
+        Toast.makeText(
+          this,
+          "Permissions not granted by the user.",
+          Toast.LENGTH_SHORT
+        ).show()
+        finish()
+      }
+    }
+  }
+
+  private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+    ContextCompat.checkSelfPermission(
+      baseContext, it
+    ) == PackageManager.PERMISSION_GRANTED
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    cameraExecutor.shutdown()
+  }
+
+  companion object {
+    private const val TAG = "CameraXApp"
+    private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private const val REQUEST_CODE_PERMISSIONS = 10
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+  }
+
+  fun ImageProxy.convertImageProxyToBitmap(): Bitmap {
+    val buffer = planes[0].buffer
+    buffer.rewind()
+    val bytes = ByteArray(buffer.capacity())
+    buffer.get(bytes)
+    return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
   }
 }
